e7f795c7e42726ddf9ccf69314a6ac6c4795fde1
Rohan Singh <rs7289579@gmail.com>
2022-11-15 22:33:16 +0530

    adding forgot login and singup pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
 import Home from "./components/Home";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Login from "./components/login/Login";
+import Singup from "./components/login/Singup";
+import Forgot from "./components/login/Forgot";
 
 function App() {
   return (
-    <>
-    <Home/>
-    </>
+    <BrowserRouter>
+    <Routes>
+      <Route path={'/login'} element={<Login/>}  />
+      <Route path={'/singup'} element={<Singup />}  />
+      <Route path={'/forgot'} element={<Forgot />}  />
+      <Route path={'/'} element={<Home/>}  />
+    </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/login/Forgot.jsx
@@ -0,0 +1,100 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Forgot = () => {
+  return (
+    <div className="w-full h-[100vh] flex flex-col md:flex-row justify-evenly items-center">
+      <div className="hidden md:block w-1/2 h-full bg-black">
+        <div className="h-full w-full grid place-items-center">
+          <div>
+            <h1 className="text-white text-5xl">Six Sense Mobility</h1>
+            <p className="text-gray-400 text-right mt-1 mr-1">company tagline here</p>
+          </div>
+        </div>
+      </div>
+
+      {/* right side forgot section */}
+      <section className="md:bg-gray-50 dark:bg-gray-900 w-full md:w-1/2 bg-black text-white h-full">
+        <h3 className="text-center text-3xl font-semibold mt-20 md:hidden">
+          Six Sense Mobility
+        </h3>
+        <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0 mt-10 md:mt-0">
+          <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+            <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+              <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                Recover account and credentials
+              </h1>
+              <form className="space-y-4 md:space-y-6" action="#">
+                {/* email address  */}
+                <div className="relative z-0 mb-6 w-full group">
+                  <input
+                    type="email"
+                    name="floating_email"
+                    id="floating_email"
+                    className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-black peer"
+                    placeholder=" "
+                    required
+                  />
+                  <label
+                    htmlFor="floating_email"
+                    className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-black peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+                  >
+                    Email address
+                  </label>
+                </div>
+                {/* new password  */}
+                <div className="relative z-0 mb-6 w-full group">
+                  <input
+                    type="password"
+                    name="floating_password"
+                    id="floating_password"
+                    className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-black peer"
+                    placeholder=" "
+                    required
+                  />
+                  <label
+                    htmlFor="floating_password"
+                    className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-black peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+                  >
+                    New Password
+                  </label>
+                </div>
+                {/* confirm password  */}
+                <div className="relative z-0 mb-6 w-full group">
+                  <input
+                    type="password"
+                    name="floating_password"
+                    id="floating_password"
+                    className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-black peer"
+                    placeholder=" "
+                    required
+                  />
+                  <label
+                    htmlFor="floating_password"
+                    className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-black peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+                  >
+                    Confirm Password
+                  </label>
+                </div>
+                <button type="submit" className="w-full btn">
+                  Confirm
+                </button>
+                <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+                  Remember password ?{" "}
+                  <Link
+                    to={'/login'}
+                    className="font-medium text-primary-600 hover:underline dark:text-primary-500"
+                  >
+                    Sign in
+                  </Link>
+                </p>
+              </form>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Forgot;
