38607814cc3febaa482fedbc94d6fa1058215543
Rohan SIngh <rs7289579@gmail.com>
2022-12-01 01:42:47 +0530

    initial commit

--- src/components/navbar/SidebarItems.jsx
+++ src/components/navbar/SidebarItems.jsx
@@ -1,22 +1,33 @@
-import React from 'react'
-import { RiDeleteBackFill } from 'react-icons/ri'
+import React, { useContext } from "react";
+import { RiDeleteBackFill } from "react-icons/ri";
+import AppContext from "../../context/AppContext";
 
 const SidebarItems = (props) => {
+  const { state } = useContext(AppContext);
   return (
-    <div className='hover:bg-gray-300 flex items-center'>
-    <label onClick={()=> props.setDevice(props.title)} className="label cursor-pointer px-6 py-4 w-5/6">
-    <span className="label-text capitalize">{props.title}</span>
-    <input
-      type="radio"
-      name="radio"
-      className="radio checked:bg-primary"
-    />
-  </label>
-  <div className="tooltip tooltip-right capitalize" data-tip={`delete ${props.title}`}>
-  <RiDeleteBackFill onClick={() => props.handleDelete(props.title)} className="text-2xl text-red-500 cursor-pointer" />
-  </div>
-  </div>
-  )
-}
+    <div key={props.title} className="hover:bg-gray-300 flex items-center">
+      <label
+        onClick={() => props.setDevice(props.title)}
+        className="label cursor-pointer px-6 py-4 w-5/6"
+      >
+        <span className="label-text">{props.title}</span>
+        <input
+          type="checkbox"
+          className="toggle toggle-primary"
+          checked={props.title === state.currentDevice ? true : false}
+        />
+      </label>
+      <div
+        className="tooltip tooltip-right capitalize"
+        data-tip={`delete ${props.title}`}
+      >
+        <RiDeleteBackFill
+          onClick={() => props.handleDelete(props.title)}
+          className="text-2xl text-red-500 cursor-pointer"
+        />
+      </div>
+    </div>
+  );
+};
 
-export default SidebarItems
+export default SidebarItems;
